d77f9516c9090b376d70763e168068b06fcdc7cf
Görkem Mülayim <gorkemmulayim@gmail.com>
2016-08-22 14:23:48 +0300

    Upload new file

---
+++ FoxProjectv2/src/FoxZipDemo.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Demo class for FoxZip class.
+ */
+public class FoxZipDemo {
+    public static void main(String[] args) throws IOException {
+        FoxZip fz = new FoxZip();
+        File[] files = new File[3];
+        files[0] = new File("front.c");
+        files[1] = new File("odev3.pdf");
+        files[2] = new File("soru1.c");
+        File zipFile = fz.createZipFile(files);
+        System.out.println(zipFile.getName());
+    }
+}
