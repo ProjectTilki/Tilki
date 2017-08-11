58a4229c637977ce8b75d8de5010d24deac136b0
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-08-11 22:15:43 +0300

    Removed java related files and folders. Added gitignore file for node projects.

--- src/main/java/com/kasirgalabs/tilki/server/Service.java
+++
@@ -1,9 +0,0 @@
-package com.kasirgalabs.tilki.server;
-
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-
-public interface Service {
-
-    void serve() throws IOException, NoSuchAlgorithmException;
-}
--- src/main/java/com/kasirgalabs/tilki/utils/ExamList.java
+++
@@ -1,7 +0,0 @@
-package com.kasirgalabs.tilki.utils;
-
-import java.util.ArrayList;
-
-public class ExamList extends ArrayList<Exam> {
-
-}
