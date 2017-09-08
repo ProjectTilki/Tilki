d5d95edc531a28db871425ea559a1dbf3a9916bd
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-09-08 20:16:23 +0300

    Improved login error messages.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function loginForm() {
-  document.getElementById("notice").innerHTML = "Connecting...";
+  var notice = document.getElementById("notice");
+  notice.className = "connecting";
+  notice.innerHTML = "Connecting...";
+  
   var request = require('request');
 
   var options = {
-    uri: 'https://tilki-server-rootg.c9users.io/api/v1/students',
+    url: 'https://tilki-server-rootg.c9users.io/api/v1/students',
     method: 'POST',
     json: {
       "name": document.getElementById("name").value,
@@ -13,15 +16,31 @@ function loginForm() {
   };
 
   var x = request(options, function(error, response, body) {
-    if(!error && response.statusCode == 200) {
-      if(body.errors) {
-        var errors = "";
-        for(var i = 0; i < body.errors.length - 1; i++) {
-          errors += body.errors[i] + ".<br>";
-        }
-        errors += body.errors[body.errors.length - 1];
-          document.getElementById("notice").innerHTML = errors + ".";
+    if(error) {
+      if(navigator.onLine) {
+        console.error("Sorry, something went wrong.", error);
       }
+      notice.className = "error";
+      notice.innerHTML = "There is no Internet connection.";
+      return;
     }
+    if(response.statusCode != 200) {
+      notice.className = "error";
+      notice.innerHTML = response.statusCode + " " + response.statusMessage.toLowerCase() + ".";
+      return;
+    }
+    if(body.errors) {
+      var errors = "";
+      for(var i = 0; i < body.errors.length - 1; i++) {
+        errors += body.errors[i] + ".<br>";
+      }
+      errors += body.errors[body.errors.length - 1];
+      notice.className = "error";
+      notice.innerHTML = errors + ".";
+    }
+    notice.innerHTML = "";
+    notice.className = "";
+    return;
   });
 }
+
