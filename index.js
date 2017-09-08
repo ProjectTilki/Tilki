d7d45a238fac53f461efe41ec76a95f79afd2aa6
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-09-08 17:42:28 +0300

    Added login error messages.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 function loginForm() {
-  var loginForm = document.getElementById("login-form")
-  console.log(loginForm);
+  document.getElementById("notice").innerHTML = "Connecting...";
+  var request = require('request');
+
+  var options = {
+    uri: 'https://tilki-server-rootg.c9users.io/api/v1/students',
+    method: 'POST',
+    json: {
+      "name": document.getElementById("name").value,
+      "surname": document.getElementById("surname").value,
+      "number": document.getElementById("number").value
+    }
+  };
+
+  var x = request(options, function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+      if(body.errors) {
+        var errors = "";
+        for(var i = 0; i < body.errors.length - 1; i++) {
+          errors += body.errors[i] + ".<br>";
+        }
+        errors += body.errors[body.errors.length - 1];
+          document.getElementById("notice").innerHTML = errors + ".";
+      }
+    }
+  });
 }
