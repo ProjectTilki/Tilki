55f6d6d2431bf578136bf9059aa3c1ee17e6ba53
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-09-12 14:38:32 +0300

    Renamed index.* files to login.*

--- login.js
+++ login.js
@@ -1,9 +1,15 @@
+const { remote, BrowserWindow } = require('electron')
+
+const request = require('request')
+
+const path = require('path')
+const url = require('url')
+
 function loginForm () {
   let notice = document.getElementById('notice')
   notice.className = 'connecting'
   notice.innerHTML = 'Connecting...'
   
-  let request = require('request')
   
   let options = {
     url: 'https://tilki-server-rootg.c9users.io/api/v1/students',
@@ -19,9 +25,10 @@ function loginForm () {
     if (error) {
       if (navigator.onLine) {
         console.error('Sorry, something went wrong.', error)
+      } else {
+        notice.className = 'error'
+        notice.innerHTML = 'There is no Internet connection.'
       }
-      notice.className = 'error'
-      notice.innerHTML = 'There is no Internet connection.'
       return
     }
     if (response.statusCode !== 200) {
@@ -37,10 +44,17 @@ function loginForm () {
       errors += body.errors[body.errors.length - 1]
       notice.className = 'error'
       notice.innerHTML = errors + '.'
+      return
     }
     notice.innerHTML = ''
     notice.className = ''
-    return
+
+    let currentWindow = remote.getCurrentWindow()
+    currentWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'exams.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
   })
 }
 
