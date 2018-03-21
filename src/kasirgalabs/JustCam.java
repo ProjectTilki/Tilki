4222f2f2b1dce8f4d5c676548ff496068938390f
goksu <goksu@begum>
2018-03-21 17:16:34 +0200

    Sadece kamera özelliği geldi

---
+++ src/kasirgalabs/JustCam.java
@@ -0,0 +1,45 @@
+package kasirgalabs;
+import java.awt.image.BufferedImage;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+public class JustCam {
+    Mat mat = new Mat();
+    BufferedImage img;
+    byte[] dat;
+     VideoCapture cap;
+   
+    static{
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public JustCam(){
+        cap = new VideoCapture();
+        cap.open(0);
+    } 
+ 
+    BufferedImage getOneFrame() {
+        cap.read(mat);
+        return getImage(mat);
+    }
+  
+    public void getSpace(Mat mat) {
+        this.mat = mat;
+        int w = mat.cols(), h = mat.rows();
+        if (dat == null || dat.length != w * h * 3)
+            dat = new byte[w * h * 3];
+        if (img == null || img.getWidth() != w || img.getHeight() != h
+            || img.getType() != BufferedImage.TYPE_3BYTE_BGR)
+                img = new BufferedImage(w, h, 
+                            BufferedImage.TYPE_3BYTE_BGR);
+        }
+        BufferedImage getImage(Mat mat){
+            getSpace(mat);
+            mat.get(0, 0, dat);
+            img.getRaster().setDataElements(0, 0, 
+                               mat.cols(), mat.rows(), dat);
+        return img;
+    }
+  
+}
---
+++ src/kasirgalabs/JustCamFrame.java
@@ -0,0 +1,65 @@
+package kasirgalabs;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import org.opencv.core.Mat;
+
+public class JustCamFrame extends JFrame {
+
+    private JPanel contentPane;
+
+    /**
+     * Create the frame.
+     */
+    public JustCamFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 650, 490);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        Runnable repaint = new Runnable() {
+            @Override
+            public void run() {
+                for(;;) {
+                    repaint();
+                    try {
+                        Thread.sleep(30);
+                    }
+                    catch(InterruptedException e) {
+                    }
+                }
+            }
+        };
+        Thread t1 = new Thread(repaint);
+        t1.start();
+//        Runnable takePhoto = new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        Mat frame1 = new Mat();
+//                        capture.read(frame1);
+//                        takePicture(frame1);
+//                        try {
+//                            Thread.sleep(600000);// 10 dk
+//                        }
+//                        catch(InterruptedException e) {
+//                            // TODO Auto-generated catch block
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                };
+//                Thread t2 = new Thread(takePhoto);
+//                t2.start();
+    }
+
+    JustCam videoCap = new JustCam();
+
+    public void paint(Graphics g) {
+        g = contentPane.getGraphics();
+        g.drawImage(videoCap.getOneFrame(), 0, 0, this);
+    }
+
+}
