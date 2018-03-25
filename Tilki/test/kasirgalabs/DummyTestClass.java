066c131a77e71068b988804ad1e4e94e3b675aa2
nuyageva <mucahidcelik@etu.edu.tr>
2018-03-25 19:15:46 +0300

    A dummy test class added to project for testing ant build #19

---
+++ Tilki/test/kasirgalabs/DummyTestClass.java
@@ -0,0 +1,19 @@
+package kasirgalabs;
+
+import org.junit.*;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author nuyageva
+ */
+public class DummyTestClass {
+    
+    public DummyTestClass() {
+    }
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
